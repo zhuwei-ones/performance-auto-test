da6640c0d6a040f4c11d6ed4c8d970e7190dcbc8
zhuwei-ones <zhuwei@ones.cn>
2023-03-28 14:57:04 +0800

    refactor: 日志完善

--- src/utils/log.js
+++ src/utils/log.js
@@ -15,12 +15,21 @@ const levelColorMap = {
   success: 'green'
 };
 
-const myFormat = printf(({
-  level, message, label: currentLabel
-}) => {
-  const color = levelColorMap[level];
-  return chalk[color](`[${new Date().toLocaleString()}] [${currentLabel}] ${level}: ${message}`);
-});
+const myFormat = (type)=>{
+  return printf(({
+    level, message, label: currentLabel, ...rest
+  }) => {
+    const color = levelColorMap[level];
+    let restString = JSON.stringify(rest, undefined, 2);
+    restString = restString === '{}' ? '' : restString;
+
+    const result = `[${new Date().toLocaleString()}] [${currentLabel}] ${level}: ${message} ${restString}`;
+
+    return type === 'console' ? chalk[color](
+      result
+    ) : result;
+  });
+};
 
 export const logger = createLogger({
   levels: {
@@ -29,15 +38,22 @@ export const logger = createLogger({
     info: 2,
     success: 2
   },
-  format: combine(
-    label({ label: PkgJson.name }),
-    timestamp(),
-    myFormat
-  ),
-  transports: [new transports.Console()]
+  transports: [
+    // (info)=>myFormat({ ...info, type: 'console' })
+    new transports.Console({
+      format: combine(label({ label: PkgJson.name }), timestamp(), format.prettyPrint(), myFormat('console'))
+    }),
+    new transports.File({
+      filename: 'logs/performance-error.log',
+      level: 'error',
+      format: combine(label({ label: PkgJson.name }), timestamp(), format.prettyPrint(), myFormat('file'))
+    }),
+    new transports.File({
+      filename: 'logs/performance-log.log',
+      format: combine(label({ label: PkgJson.name }), timestamp(), format.prettyPrint(), myFormat('file'))
+    })
+  ]
 });
 
 // 打印测试链接以及配置
-export const printfPerformanceTestContent = ()=>{
-
-};
+export const printfPerformanceTestContent = () => {};
